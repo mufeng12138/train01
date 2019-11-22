3a2d608ba89197c769ffe2c0702705a1816f940b
mufeng12138 <392041634@qq.com>
2019-11-22 15:19:53 +0800

    20191122_ui_test2

---
+++ venv/main.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+# author = "mufeng"
+# title = 摘取网页上全部邮箱
+
+from selenium import webdriver
+import re
+import logging
+
+# LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+LOG_FORMAT = '%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s'
+DATE_FORMAT = '%a, %d %b %Y %H:%M:%S'
+# DATE_FORMAT = "%m/%d/%Y  &H:%M:%S  %P"
+# logging.basicConfig(filename = "my.log",
+#                     level= logging.DEBUG,
+#                     format = LOG_FORMAT,
+#                     datefmt = DATE_FORMAT)
+
+logging.basicConfig(filename = "my.log",
+                    level = logging.INFO,
+                    format = LOG_FORMAT,
+                    datefmt = DATE_FORMAT)
+
+url = r"http://home.baidu.com/contact.html"
+
+def main():
+    # print("hello mf")
+    driver = webdriver.Chrome()
+    logging.info("open browser")
+    # logging.debug("User %s is loging" % 'jeck')
+    # logging.info("User %s attempted wrong password" % 'fuzj')
+    # logging.warning("user %s attempted wrong password more than 3 times" % 'mary')
+    # logging.error("select db is timeout")
+    # logging.critical("server is down")
+    driver.get(url)
+    # logging.info("link url")
+    doc = driver.page_source
+    # logging.info("get page")
+    emails = re.findall(r'[\w]+@[\w\.-]+', doc)
+    for email in emails:
+        print(email)
+    # driver.close()
+
+if __name__ == "__main__" :
+    main()
